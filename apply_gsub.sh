32092138555ccd4767aad2f57bd00a308adea5ce
LiNk-NY <mramos09@gmail.com>
2018-08-21 21:31:48 -0400

    fix script path

--- apply_gsub.sh
+++ apply_gsub.sh
@@ -8,10 +8,14 @@ cd $BIOC
 
 readarray -t PKGS < $BIOC/$GIST_FOLDER/$LIST_FILE
 
+# testing
+# for i in ${PKGS[@]:0:5}
+
 for i in ${PKGS[@]}
 do
     cd $BIOC/git.bioconductor.org/software/$i
-    ./gsub_biocLite.sh $i
+    $BIOC/utils/gsub_biocLite.sh $i
 done
 
 cd $BIOC
+
