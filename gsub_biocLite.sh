99743d47e1a1d05b41ee447a395775f74a205fb5
LiNk-NY <mramos09@gmail.com>
2018-09-05 15:30:04 -0400

    echo only if hits found

--- gsub_biocLite.sh
+++ gsub_biocLite.sh
@@ -15,32 +15,51 @@ BIOCLITE_CALL_REGEXP="(.*)$LITE_CALL(\(.*\)*)$"
 
 library_hits=`find . ! -path . -regex "$SOURCE_FILES" -exec grep -El "$LIBRARY_LINE_REGEXP" {} \+`
 
-echo "Replacing any BiocInstaller with BiocManager..."
+if [ ! -z "${library_hits// }" ]; then
+    echo "Replacing any library\(BiocInstaller\) with BiocManager..."
 
-for i in $library_hits;
-do
-    sed -i "s/\<$BIOC_INST\>/BiocManager/" $i
-done
+    for i in $library_hits;
+    do
+        sed -i "s/\<$BIOC_INST\>/BiocManager/" $i
+    done
+fi
 
 ## for DESCRIPTION files
 sed -i "s/\<$BIOC_INST\>/BiocManager/" $DESC_FILE
 
 source_hits=`find . ! -path . -regex "$SOURCE_FILES" -exec grep -El "$SOURCE_LINE_REGEXP" {} \+`
 
-echo "Replacing source(*/biocLite.R) with install.packages('BiocManager')"
-for i in $source_hits;
-do
-    sed -E -i "s|$SOURCE_LINE_REGEXP|$BIOC_MGR|" $i
-done
+if [ ! -z "${source_hits// }" ]; then
+    echo "Replacing source('.../biocLite.R') with install.packages('BiocManager')"
+
+    for i in $source_hits;
+    do
+        sed -E -i "s|$SOURCE_LINE_REGEXP|$BIOC_MGR|" $i
+    done
+fi
+
+code_hits=`find . ! -path . -regex "$SOURCE_FILES" -exec grep -El "$CODE_BIOCLITE_REGEXP" {} \+`
+
+if [ ! -z "${code_hits// }" ]; then
+    echo "Replacing source(.../biocLite.R) with install.packages"
+
+    for i in $code_hits;
+    do
+        sed -E -i "s|$CODE_BIOCLITE_REGEXP|$CODE_BIOCLITE|" $i
+    done
+fi
 
 biocLite_hits=`find . ! -path . -regex "$SOURCE_FILES" -exec grep -El "$BIOCLITE_CALL_REGEXP" {} \+`
 
-echo "Replacing biocLite() with BiocManager::install()"
-for i in $biocLite_hits;
-do
-    sed -i "s/\<$FULL_CALL_REGEXP\>/$MGR_INST/" $i
-    sed -E -i "s|$BIOCLITE_CALL_REGEXP|\1$MGR_INST\2|" $i
-done
+if [ ! -z "${biocLite_hits// }" ]; then
+    echo "Replacing biocLite() with BiocManager::install()"
+
+    for i in $biocLite_hits;
+    do
+        sed -i "s/\<$FULL_CALL_REGEXP\>/$MGR_INST/" $i
+        sed -E -i "s|$BIOCLITE_CALL_REGEXP|\1$MGR_INST\2|g" $i
+    done
+fi
 
 LITE_FILES="$library_hits $source_hits $biocLite_hits"
 
