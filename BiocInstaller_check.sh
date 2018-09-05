50f996d77927c77963d4cf23a65520d9abf51357
LiNk-NY <mramos09@gmail.com>
2018-09-05 18:55:49 -0400

    exclude scripts directory

--- BiocInstaller_check.sh
+++ BiocInstaller_check.sh
@@ -20,6 +20,7 @@ do
     find . -name '.git' -type d -prune \
         -o -name 'data' -type d -prune \
         -o -name 'doc' -type d -prune \
+        -o -name 'scripts' -type d -prune \
         -o -regextype posix-egrep -regex ".*(\.[Rr]?[DdNnMmWw]*|NAMESPACE|DESCRIPTION)$" -type f -print0 | \
         xargs -0 grep -EI --color \
         --exclude={*\.[Rr][Dd][AaSs],*\.[Rr][Dd]ata,*\.Rhistory,*\.txt,*NEWS*,*\.html,Makefile,\.travis\.yml} \
