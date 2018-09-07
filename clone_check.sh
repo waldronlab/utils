742378320ca1b26c3aad35071b5f16656076f3ae
LiNk-NY <mramos09@gmail.com>
2018-09-07 13:57:56 -0400

    add simple clone and grep check script

---
+++ clone_check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PKGNAME=$1
+
+git clone git@git.bioconductor.org:packages/$PKGNAME.git && cd $PKG
+
+find . -name '.git' -type d -prune \
+    -o -name 'data' -type d -prune \
+    -o -name 'doc' -type d -prune \
+    -o -name 'scripts' -type d -prune \
+    -o -regextype posix-egrep -regex ".*(\.[Rr]?[DdNnMmWw]*|NAMESPACE|DESCRIPTION)$" -type f -print0 | \
+    xargs -0 grep -EI --color \
+    --exclude={*\.[Rr][Dd][AaSs],*\.[Rr][Dd]ata,*\.Rhistory,*\.txt,*NEWS*,*\.html,Makefile,\.travis\.yml} \
+    'BiocInstaller|biocLite|biocValid|biocVersion|biocinstallRepos|useDevel'
+
