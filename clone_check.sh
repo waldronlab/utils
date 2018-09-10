8b6975548933c558acd6f6a20663f3df594f25da
LiNk-NY <marcel.ramosperez@roswellpark.org>
2018-09-10 18:41:36 +0000

    use find_keywords.sh script and update gitclone.sh

--- clone_check.sh
+++ clone_check.sh
@@ -4,12 +4,5 @@ PKGNAME=$1
 
 git clone git@git.bioconductor.org:packages/$PKGNAME.git && cd $PKGNAME
 
-find . -name '.git' -type d -prune \
-    -o -name 'data' -type d -prune \
-    -o -name 'doc' -type d -prune \
-    -o -name 'scripts' -type d -prune \
-    -o -regextype posix-egrep -regex ".*(\.[Rr]?[DdNnMmWw]*|NAMESPACE|DESCRIPTION)$" -type f -print0 | \
-    xargs -0 grep -EI --color \
-    --exclude={*\.[Rr][Dd][AaSs],*\.[Rr][Dd]ata,*\.Rhistory,*\.txt,*NEWS*,*\.html,Makefile,\.travis\.yml} \
-    'BiocInstaller|biocLite|biocValid|biocVersion|biocinstallRepos|useDevel'
+./utils/find_keywords.sh
 
