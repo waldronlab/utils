8bfaef93594fbc84dda60f35eeb1f78191ed7849
LiNk-NY <marcel.ramosperez@roswellpark.org>
2019-10-26 10:23:25 -0400

    remove bak file from sed

--- version_bump.sh
+++ version_bump.sh
@@ -21,5 +21,5 @@ echo "==> $new_version_string ($action)"
 
 new_line="Version: $new_version_string"
 
-sed -i.bak "s/$old_line/$new_line/" "$desc_file"
+sed -i "s/$old_line/$new_line/" "$desc_file"
 
